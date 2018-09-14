e7b7cf7ac39e4ca82a2cfb8b02bf1cc2542e6e29
marcoabo <marcoabo@yahoo.com>
2018-09-14 08:20:31 -0300

    Check mate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				
@@ -47,6 +47,8 @@ public class Program {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
-		}		
+		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
